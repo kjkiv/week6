8380c537f73f17a6620ab404684e0c25c197f54d
Karl Kaufmann <kjkiv@yahoo.com>
2022-11-13 14:33:22 -0500

    commit week6

--- week6.js
+++ week6.js
@@ -1,3 +1,59 @@
-console.log("this is my first commit")
-console.log("this is my second commit")
+const SUITS = ["S", "C", "H", "D"]
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
+
+ export default class Deck{
+
+    constructor(cards = freshDeck()) {
+        this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+       for (let i = this.numberOfCards -1; i > 0; i--) {
+        const newIndex = Math.Floor(Math.random() * (i + 1))
+        const oldValue = this.cards[newIndex]
+        this.cards[newIndex] = this.cards[i]
+        this.cards[i] = oldValue
+       }
+    }
+}
+
+class Card {
+   constructor(suit, value) {
+    this.suit = suit
+    this.value = value
+   } 
+}
+
+
+
+
+
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
+}
+
+let playerDeck, computerDeck
+
+startGame(
+    function startGame() {
+        const deck = new Deck()
+        deck. shuffle()
+
+        const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+        playerDeck = new Deck (deck.cards.slice(0, deckMidpoint))
+        computerDeck = new Deck (deck.cards.slice(deckMidpoint, deck.numberOfCards))
+
+        
+
+
+    }
+)
